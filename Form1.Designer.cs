dc0c468bcb1004b1235d08d10e9eaf8016e1b309
fishca <sergrud@mail.ru>
2018-12-20 16:29:53 +0300

    Добавлен пакет Nlog

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
@@ -41,21 +42,36 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(1, 314);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(801, 136);
+            this.textBox1.TabIndex = 1;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Чтение 1С файлов";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
+using System.Collections.Specialized;
 
 namespace MetaRead
 {
@@ -66,6 +67,94 @@ namespace MetaRead
                 //}
             }
 
+            textBox1.Text += ("Новая текстовая строка"+Environment.NewLine);
+
+
+
         }
     }
+
+    public class Messager : MessageRegistrator
+    {
+        private string LogFile;
+        private TextBox MemoLog;
+        private string _s;
+
+        public Messager()
+        {
+        }
+
+        public Messager(string _logfile, TextBox _MemoLog)
+        {
+            SetLogFile(_logfile);
+            MemoLog = _MemoLog;
+        }
+
+        public void SetLogFile(string _logfile)
+        {
+            if (_logfile.Length > 0)
+            {
+                LogFile = Path.GetFullPath(_logfile);
+                if (File.Exists(LogFile))
+                    File.Delete(LogFile);
+            }
+            else
+                LogFile = _logfile;
+
+        }
+
+        public override void AddMessage(string message, MessageState mstate, StringCollection param = null)
+        {
+            string s = "";
+            s = DateTime.Now.ToString();
+            s += " ";
+            switch (mstate)
+            {
+                case MessageState.msEmpty:
+                    s += "<>";
+                    break;
+                case MessageState.msSuccesfull:
+                    s += "<ok>";
+                    break;
+                case MessageState.msWarning:
+                    s += "<warning>";
+                    break;
+                case MessageState.msInfo:
+                    s += "<info>";
+                    break;
+                case MessageState.msError:
+                    s += "<error>";
+                    break;
+                case MessageState.msWait:
+                    s += "<wait>";
+                    break;
+                case MessageState.msHint:
+                    s += "<hint>";
+                    break;
+                default:
+                    s += "<>";
+                    break;
+            }
+            s += " ";
+            s += message;
+            if (param != null)
+            {
+                for (int i = 0; i < param.Count; i++)
+                {
+                    s += (Environment.NewLine + "\t");
+                    s += param[i];
+                }
+            }
+            s += Environment.NewLine;
+            _s = s;
+        }
+
+        public override void Status(string message)
+        {
+            base.Status(message);
+        }
+
+    }
+
+
 }
