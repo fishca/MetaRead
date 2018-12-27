429fc9766b84a96d592d90a7501eadd2a8836567
fishca <sergrud@mail.ru>
2018-12-28 00:00:08 +0300

    Доработка новых классов

--- Value1C.cs
+++ Value1C.cs
@@ -120,13 +120,364 @@ namespace MetaRead
         }
     }
 
-    public class Value1C<T> : Value1C
+    public class Value1C_number : Value1C
     {
-        public T v_value;
-        public Value1C(Value1C_obj _parent) : base(_parent)
+        public string v_string;
+        public int v_number;
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="_parent"></param>
+        public Value1C_number(Value1C_obj _parent) : base(_parent)
         {
         }
+
+        public override string presentation(bool english = false)
+        {
+            return "";
+        }
+
+        public override string valuepresentation(bool english = false)
+        {
+            return "";
+        }
+
+        public override bool Export(string path, StreamWriter str, int indent, bool english = false)
+        {
+            return false;
+        }
+
+        public override bool IsEmpty()
+        {
+            return false;
+        }
     }
+
+    public class Value1C_number_exp : Value1C
+    {
+        public string v_string;
+        public decimal v_number;
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="_parent"></param>
+        public Value1C_number_exp(Value1C_obj _parent) : base(_parent)
+        {
+        }
+
+        public override string presentation(bool english = false)
+        {
+            return "";
+        }
+
+        public override string valuepresentation(bool english = false)
+        {
+            return "";
+        }
+
+        public override bool Export(string path, StreamWriter str, int indent, bool english = false)
+        {
+            return false;
+        }
+
+        public override bool IsEmpty()
+        {
+            return false;
+        }
+    }
+
+    public class Value1C_date : Value1C
+    {
+        // static char emptydate[7];
+        public static string emptydate;
+        // unsigned char v_date[7];
+        public string v_date;
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="_parent"></param>
+        public Value1C_date(Value1C_obj _parent) : base(_parent)
+        {
+        }
+
+        public override string presentation(bool english = false)
+        {
+            return "";
+        }
+
+        public override string valuepresentation(bool english = false)
+        {
+            return "";
+        }
+
+        public override bool Export(string path, StreamWriter str, int indent, bool english = false)
+        {
+            return false;
+        }
+
+        public override bool IsEmpty()
+        {
+            return false;
+        }
+    }
+
+    public class Value1C_null : Value1C
+    {
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="_parent"></param>
+        public Value1C_null(Value1C_obj _parent) : base(_parent)
+        {
+        }
+
+        public override string presentation(bool english = false)
+        {
+            return "";
+        }
+
+        public override string valuepresentation(bool english = false)
+        {
+            return "";
+        }
+
+        public override bool Export(string path, StreamWriter str, int indent, bool english = false)
+        {
+            return false;
+        }
+
+        public override bool IsEmpty()
+        {
+            return false;
+        }
+    }
+
+    public class Value1C_undef : Value1C
+    {
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="_parent"></param>
+        public Value1C_undef(Value1C_obj _parent) : base(_parent)
+        {
+        }
+
+        public override string presentation(bool english = false)
+        {
+            return "";
+        }
+
+        public override string valuepresentation(bool english = false)
+        {
+            return "";
+        }
+
+        public override bool Export(string path, StreamWriter str, int indent, bool english = false)
+        {
+            return false;
+        }
+
+        public override bool IsEmpty()
+        {
+            return false;
+        }
+    }
+
+    public class Value1C_type : Value1C
+    {
+        public MetaType v_type;
+        public Guid v_uid;
+        
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="_parent"></param>
+        public Value1C_type(Value1C_obj _parent) : base(_parent)
+        {
+        }
+
+        public override string presentation(bool english = false)
+        {
+            return "";
+        }
+
+        public override string valuepresentation(bool english = false)
+        {
+            return "";
+        }
+
+        public override bool Export(string path, StreamWriter str, int indent, bool english = false)
+        {
+            return false;
+        }
+
+        public override bool IsEmpty()
+        {
+            return false;
+        }
+    }
+
+    public class Value1C_uid : Value1C
+    {
+        public Guid v_uid;
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="_parent"></param>
+        public Value1C_uid(Value1C_obj _parent) : base(_parent)
+        {
+        }
+
+        public override string presentation(bool english = false)
+        {
+            return "";
+        }
+
+        public override string valuepresentation(bool english = false)
+        {
+            return "";
+        }
+
+        public override bool Export(string path, StreamWriter str, int indent, bool english = false)
+        {
+            return false;
+        }
+
+        public override bool IsEmpty()
+        {
+            return false;
+        }
+    }
+
+    public class Value1C_enum : Value1C
+    {
+        public MetaValue v_enum; // Значение системного перечисления
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="_parent"></param>
+        public Value1C_enum(Value1C_obj _parent) : base(_parent)
+        {
+        }
+
+        public override string presentation(bool english = false)
+        {
+            return "";
+        }
+
+        public override string valuepresentation(bool english = false)
+        {
+            return "";
+        }
+
+        public override bool Export(string path, StreamWriter str, int indent, bool english = false)
+        {
+            return false;
+        }
+
+        public override bool IsEmpty()
+        {
+            return false;
+        }
+    }
+
+    public class Value1C_stdattr : Value1C
+    {
+        public MetaValue v_enum; // Значение системного перечисления
+        public MetaObject v_metaobj; // Объект метаданных
+        public MetaStandartAttribute v_stdattr;
+        public int v_value;
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="_parent"></param>
+        public Value1C_stdattr(Value1C_obj _parent) : base(_parent)
+        {
+        }
+
+        public override string presentation(bool english = false)
+        {
+            return "";
+        }
+
+        public override string valuepresentation(bool english = false)
+        {
+            return "";
+        }
+
+        public override bool Export(string path, StreamWriter str, int indent, bool english = false)
+        {
+            return false;
+        }
+
+        public override bool IsEmpty()
+        {
+            return false;
+        }
+    }
+
+    public class Value1C_stdtabsec : Value1C
+    {
+        public MetaObject v_metaobj; // Объект метаданных
+        public MetaStandartTabularSection v_stdtabsec;
+        public int v_value;
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="_parent"></param>
+        public Value1C_stdtabsec(Value1C_obj _parent) : base(_parent)
+        {
+        }
+
+        public override string presentation(bool english = false)
+        {
+            return "";
+        }
+
+        public override string valuepresentation(bool english = false)
+        {
+            return "";
+        }
+
+        public override bool Export(string path, StreamWriter str, int indent, bool english = false)
+        {
+            return false;
+        }
+
+        public override bool IsEmpty()
+        {
+            return false;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // public class Value1C<T> : Value1C
+    // {
+    //     public T v_value;
+    //     public Value1C(Value1C_obj _parent) : base(_parent)
+    //     {
+    //     }
+    // }
 
     //////////////////////////////////////////////////////////////
 
