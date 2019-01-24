26e34ed3fda190627c8d060a76026a5701cd29a2
fishca <sergrud@mail.ru>
2019-01-24 07:35:23 +0300

    Добавлен новый файл

---
+++ source/ParserData.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetaRead
+{
+    public struct FileInfo
+    {
+        public string GUID;
+        public string Path;
+        public string Dir;
+        public string CodePage;
+        public string Version;
+        public bool IsFolder;
+    }
+
+    
+
+    public enum SortMode
+    {
+        smNone,
+        smByGUID,
+        smByPath,
+        smByDir
+    }
+
+    // public class FileInfo
+    // {
+    // }
+
+
+    public class ParserData
+    {
+    }
+}
